3584ecbbfca9001f23e057a336a64071d86c1414
Anjana Nabhan <anjana.nabhan@smartpension.co.uk>
2022-09-09 15:50:11 +0100

    create a new route, controller, and view to pass the acceptance test

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,6 +3,6 @@ require 'rails_helper'
 feature 'Homepage' do
   scenario 'see "Smartflix"' do
     visit '/'
-    expect (page). to have_content 'Smartflix'
+    expect(page).to have_content 'Smartflix'
   end
 end
