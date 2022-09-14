6a18afa4cb552e502e457d296d682ee4eae727ae
Anjana Nabhan <anjana.nabhan@smartpension.co.uk>
2022-09-14 16:10:05 +0100

    make 'Smartflix' the main header on the homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 feature 'Homepage' do
-  scenario 'see "Smartflix"' do
+  scenario 'see "Smartflix" as the main header' do
     visit '/'
-    expect(page).to have_content 'Smartflix'
+    expect(page).to have_selector('h1', text: 'Smartflix')
   end
 end
