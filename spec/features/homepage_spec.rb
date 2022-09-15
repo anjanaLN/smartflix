c7372a602f8f7ce04f4311b07be39f3450b26316
Anjana Nabhan <anjana.nabhan@smartpension.co.uk>
2022-09-15 15:57:36 +0100

    can see the titles of the first 6 shows on the homepage

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -5,4 +5,15 @@ feature 'Homepage' do
     visit '/'
     expect(page).to have_selector('h1', text: 'Smartflix')
   end
+
+  scenario 'see the titles of the first six shows' do
+    visit '/'
+    expect(page).to have_selector('h3', text: 'Dick Johnson Is Dead 2020')
+    expect(page).to have_selector('h3', text: 'Blood & Water 2021')
+    expect(page).to have_selector('h3', text: 'Ganglands 2021')
+    expect(page).to have_selector('h3', text: 'Jailbirds New Orleans 2021')
+    expect(page).to have_selector('h3', text: 'Kota Factory 2021')
+    expect(page).to have_selector('h3', text: 'Midnight Mass 2021')
+    expect(page).not_to have_selector('h3', text: 'My Little Pony: A New Generation 2021')
+  end
 end
