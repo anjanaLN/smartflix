eaef406be33a525b03c098cb248e11207f3b7f25
Anjana Nabhan <anjana.nabhan@smartpension.co.uk>
2022-09-20 16:23:13 +0100

    match on text instead of specific selectors

--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -3,17 +3,17 @@ require 'rails_helper'
 feature 'Homepage' do
   scenario 'see "Smartflix" as the main header' do
     visit '/'
-    expect(page).to have_selector('h1', text: 'Smartflix')
+    expect(page).to have_text('Smartflix')
   end
 
   scenario 'see the titles of the first six shows' do
     visit '/'
-    expect(page).to have_selector('h3', text: 'Dick Johnson Is Dead 2020')
-    expect(page).to have_selector('h3', text: 'Blood & Water 2021')
-    expect(page).to have_selector('h3', text: 'Ganglands 2021')
-    expect(page).to have_selector('h3', text: 'Jailbirds New Orleans 2021')
-    expect(page).to have_selector('h3', text: 'Kota Factory 2021')
-    expect(page).to have_selector('h3', text: 'Midnight Mass 2021')
-    expect(page).not_to have_selector('h3', text: 'My Little Pony: A New Generation 2021')
+    expect(page).to have_text('Dick Johnson Is Dead 2020')
+    expect(page).to have_text('Blood & Water 2021')
+    expect(page).to have_text('Ganglands 2021')
+    expect(page).to have_text('Jailbirds New Orleans 2021')
+    expect(page).to have_text('Kota Factory 2021')
+    expect(page).to have_text('Midnight Mass 2021')
+    expect(page).not_to have_text('My Little Pony: A New Generation 2021')
   end
 end
